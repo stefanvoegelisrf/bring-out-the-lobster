258aa273db4ae48933b5a5c8d1fc38e8bf7700a0
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-05-29 10:08:21 +0200

    add initial server

---
+++ TrialPeriodServer/TrialPeriodServer/NavigationHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+namespace TrialPeriodServer
+{
+	public class NavigationHub:Hub
+	{
+		public NavigationHub()
+		{
+		}
+
+		public async Task SendMessage(string message)
+		{
+			await Clients.All.SendAsync("ReceiveMessage", message);
+		}
+	}
+}
+
